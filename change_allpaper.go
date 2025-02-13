734962ab4d8612d049b42c748febc942b84cbe22
Shadhin <mdshadhinkn@gmail.com>
2025-02-13 17:31:52 +0600

    refactor: clean up code formatting and improve readability

--- change_allpaper.go
+++ change_allpaper.go
@@ -1,62 +1,63 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "time"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 )
 
 // Function to get all wallpapers from the folder
 func getWallpapers(folder string) ([]string, error) {
-    files, err := filepath.Glob(filepath.Join(folder, "*.jpg"))
-    if err != nil {
-        return nil, err
-    }
+	files, err := filepath.Glob(filepath.Join(folder, "*.jpg"))
+	if err != nil {
+		return nil, err
+	}
 
-    pngFiles, err := filepath.Glob(filepath.Join(folder, "*.png"))
-    if err != nil {
-        return nil, err
-    }
+	pngFiles, err := filepath.Glob(filepath.Join(folder, "*.png"))
+	if err != nil {
+		return nil, err
+	}
 
-    files = append(files, pngFiles...)
+	files = append(files, pngFiles...)
 
-    if len(files) == 0 {
-        return nil, fmt.Errorf("no wallpapers found in the folder")
-    }
+	if len(files) == 0 {
+		return nil, fmt.Errorf("no wallpapers found in the folder")
+	}
 
-    return files, nil
+	return files, nil
 }
 
 // Function to set the wallpaper using gsettings
 func setWallpaper(wallpaperPath string) error {
-    cmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", fmt.Sprintf("file://%s", wallpaperPath))
-    return cmd.Run()
+	cmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", fmt.Sprintf("file://%s", wallpaperPath))
+	return cmd.Run()
 }
 
 func main() {
-    // Define command-line flags
-    duration := flag.Int("duration", 5, "Duration in seconds between wallpaper changes")
-    path := flag.String("path", ".", "Path to the wallpapers folder")
+	// Define command-line flags
+	fmt.Println("FREE PALESTINE 🇵🇸")
+	duration := flag.Int("duration", 5, "Duration in seconds between wallpaper changes")
+	path := flag.String("path", ".", "Path to the wallpapers folder")
 
-    flag.Parse()
+	flag.Parse()
 
-    interval := time.Duration(*duration) * time.Second
-    wallpapers, err := getWallpapers(*path)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error:", err)
-        return
-    }
+	interval := time.Duration(*duration) * time.Second
+	wallpapers, err := getWallpapers(*path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		return
+	}
 
-    for {
-        for _, wallpaper := range wallpapers {
-            if err := setWallpaper(wallpaper); err != nil {
-                fmt.Fprintln(os.Stderr, "Error:", err)
-                return
-            }
-            time.Sleep(interval)
-        }
-    }
+	for {
+		for _, wallpaper := range wallpapers {
+			if err := setWallpaper(wallpaper); err != nil {
+				fmt.Fprintln(os.Stderr, "Error:", err)
+				return
+			}
+			time.Sleep(interval)
+		}
+	}
 }
